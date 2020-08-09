3de39ee290b96e7622f8d66106cfaef55f670972
Ahmed S. Tahir <astahir@gmail.com>
2020-08-09 13:30:48 +0500

    Added .travis.yml file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,6 @@ import App from './App';
 
 test('renders the component', () => {
     const component = shallow(<App />);
-    expect(component).toMatchSnapshot();
+    const elem = component.find('div');
+    expect(elem).toMatchSnapshot();
 });
